342c08343e9e7abb2f83fe59c2e8b54303e2930c
100007217 <gomezmonterroso14@gmail.com>
2021-11-19 18:01:32 +0100

    Puesto en funcion saludo y background

--- js/code.js
+++ js/code.js
@@ -1,13 +1,18 @@
-window.onload = function saludo() {
+function saludo() {
     const titulo = document.getElementById("titulo");
     const d = new Date();
     let hora = d.getHours();
     console.log(hora)
     if (hora < 7 || hora > 17) {
         titulo.innerHTML = "Buenas noches";
+        document.body.style.backgroundColor = "#3E3730"
     } else {
         titulo.innerHTML = "Buenos dias";
+        document.body.style.backgroundColor = "rgb(238, 107, 47)"
     }
+}
+window.onload = function() {
+    saludo();
 
 
 }
