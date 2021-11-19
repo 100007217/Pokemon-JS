aab741d7b65e10add162b9d6aff0c0792cbfd995
100007217 <gomezmonterroso14@gmail.com>
2021-11-19 17:53:53 +0100

    Saludo dia y noche

--- js/code.js
+++ js/code.js
@@ -2,6 +2,7 @@ window.onload = function saludo() {
     const titulo = document.getElementById("titulo");
     const d = new Date();
     let hora = d.getHours();
+    console.log(hora)
     if (hora < 7 || hora > 17) {
         titulo.innerHTML = "Buenas noches";
     } else {
